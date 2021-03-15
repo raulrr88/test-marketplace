aeb6753bf9e933457409fb514b55369163bc02ff
Raul Rizzo Ribeiro <raulrr88@gmail.com>
2021-03-14 23:08:46 -0300

    Basic GraphQL Server Setup and ESLint resolvers

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,1 +1,30 @@
-console.log('Hello Marketplace!');
+import { ApolloServer, gql } from 'apollo-server';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const PORT = process.env.PORT || 4100;
+const IS_PRODUCTION = process.env.NODE_ENV === 'production';
+
+const typeDefs = gql`
+  type Query {
+    helloMarketplace: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    helloMarketplace: () => 'Hello Marketplace',
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  playground: !IS_PRODUCTION,
+  introspection: !IS_PRODUCTION,
+  tracing: !IS_PRODUCTION,
+});
+
+server.listen(PORT).then(({ url }) => {
+  console.info(`💾 Server running at ${url}`);
+});
