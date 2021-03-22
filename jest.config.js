e714556a0db10cf32520d1e9999797172fc5a5af
Raul Rizzo Ribeiro <raulrr88@gmail.com>
2021-03-22 00:31:33 -0300

    Jest Config

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    clearMocks: true,
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    testMatch: ['**/*.spec.ts'],
+    collectCoverage: true,
+    coverageProvider: 'v8',
+    coverageDirectory: 'coverage',
+    collectCoverageFrom: ['<rootDir>/src/modules/**/services/*.ts'],
+    coverageReporters: ['text-summary', 'lcov'],
+};
